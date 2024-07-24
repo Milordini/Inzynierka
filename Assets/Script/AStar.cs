f59bc6f03cc6d7c6fbb354ed85e9ad2a1f0b44a2
Milordini <papsike124@gmail.com>
2024-07-24 20:32:37 +0200

    ugh

--- Assets/Script/AStar.cs
+++ Assets/Script/AStar.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AStar : MonoBehaviour
 {
-    
-    
 
+    private List<GameObject> closedSet;
+    private List<GameObject> openSet;
+    public bool Astar(Square start, Square goal)
+    {
+        openSet.Add(start.gameObject);
 
+        
+    }
+        
 }
+
+
+

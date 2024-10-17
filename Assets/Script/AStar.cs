24a81f28550dd29fc0279d4f5bf68304483b51a4
Milordini <papsike124@gmail.com>
2024-10-17 11:00:45 +0200

    odwiedzone punkty
    algoprytmy zwracają rózwnież odwiedzone punkty

--- Assets/Script/AStar.cs
+++ Assets/Script/AStar.cs
@@ -40,17 +40,13 @@ public class AStar
         
         return neighbors;
     }
-    public List<Square> findPath(Square start, Square end)
     {
-        List<Square> openSet = new List<Square>();
-        HashSet<Square> closedSet = new HashSet<Square>();
         openSet.Add(start);
         start.H = 0;
         start.G = 0;
 
         while (openSet.Count > 0)
         {
-        Square curent = openSet[0];
             
             for (int i = 1; i < openSet.Count; i++)
                 if (openSet[i].F < curent.F || openSet[i].F == curent.F && openSet[i].H < curent.H)
@@ -65,7 +61,6 @@ public class AStar
             if (curent == end)
                 return RetracePath(start, curent); 
 
-            foreach(Square neighbor in getNeighbors(curent))
             {
 
                 if (!neighbor.canWalk ||closedSet.Contains(neighbor))
@@ -85,6 +80,54 @@ public class AStar
         }
         return new List<Square>();
     }
+
+    public List<Square> findPath(Square start, Square end,List<Square> visitedCords)
+    {
+        List<Square> openSet = new List<Square>();
+        HashSet<Square> closedSet = new HashSet<Square>();
+        openSet.Add(start);
+        start.H = 0;
+        start.G = 0;
+
+        while (openSet.Count > 0)
+        {
+            Square curent = openSet[0];
+
+            for (int i = 1; i < openSet.Count; i++)
+                if (openSet[i].F < curent.F || openSet[i].F == curent.F && openSet[i].H < curent.H)
+                    curent = openSet[i];
+
+            openSet.Remove(curent);
+
+            //curent.gameObject.GetComponent<SpriteRenderer>().color = Color.cyan;
+            //Thread.Sleep(100);
+            visitedCords.Add(curent);
+            closedSet.Add(curent);
+
+            if (curent == end)
+                return RetracePath(start, curent);
+
+            foreach (Square neighbor in getNeighbors(curent))
+            {
+
+                if (!neighbor.canWalk || closedSet.Contains(neighbor))
+                    continue;
+
+                int movecost = curent.G + getDistance(curent, neighbor);
+
+                if (movecost < neighbor.G || !openSet.Contains(neighbor))
+                {
+                    neighbor.G = movecost;
+                    neighbor.H = getDistance(neighbor, end);
+                    neighbor.parent = curent;
+                    if (!openSet.Contains(neighbor))
+                        openSet.Add(neighbor);
+                }
+            }
+        }
+        return new List<Square>();
+    }
+
     public int getDistance(Square a, Square b)
     {
         int dstX = Mathf.Abs(a.X - b.X);
